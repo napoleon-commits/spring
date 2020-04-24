de12f49e7e8a84f40727384d19b533d5269288d7
Byron Hall <byronhall@MacBook-Pro.local>
2020-04-24 10:43:45 -0500

    modelview

--- WebApp/src/main/java/com/commits/napoleon/PlayersController.java
+++ WebApp/src/main/java/com/commits/napoleon/PlayersController.java
@@ -1,20 +1,22 @@
 package com.commits.napoleon;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PlayersController {
 	
 	@RequestMapping("/players")
-	public String players(HttpServletRequest req) {
-		HttpSession session = req.getSession();
-		String playerName = req.getParameter("playername");
-		session.setAttribute("playername", playerName);
-		return "players";
+	public ModelAndView players(@RequestParam("playername") String playerName) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("playername", playerName);
+		mv.setViewName("players");
+		return mv;
 	}
 }
