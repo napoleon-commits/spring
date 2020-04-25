f5a71865af52af69eade635b48f8f2922d959e3c
Byron Hall <byronhall@MacBook-Pro.local>
2020-04-25 00:37:49 -0500

    commented out imports

--- SubmissionForm/src/main/java/com/commits/napoleon/FormController.java
+++ SubmissionForm/src/main/java/com/commits/napoleon/FormController.java
@@ -2,8 +2,8 @@ package com.commits.napoleon;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
