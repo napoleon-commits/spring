b03245c06fb1d46d0415214b58f11c1f576d6b6f
Byron Hall <byronhall@MacBook-Pro.local>
2020-04-25 10:15:09 -0500

    spring security

---
+++ Secure/src/main/java/com/commits/napoleon/Controller.java
@@ -0,0 +1,15 @@
+package com.commits.napoleon;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Controller implements WebMvcConfigurer{
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/napoleon").setViewName("napoleon");
+		registry.addViewController("/login").setViewName("login");		
+	}
+}
---
+++ Secure/src/main/java/com/commits/napoleon/MvcConfigurer.java
@@ -0,0 +1,33 @@
+package com.commits.napoleon;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class MvcConfigurer extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/login").permitAll()
+		.and()
+		.logout().permitAll();
+	}
+	
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		@SuppressWarnings("deprecation")
+		UserDetails user = User.withDefaultPasswordEncoder().username("napoleon").password("commits").roles("USER").build();
+		return new InMemoryUserDetailsManager(user);
+	}
+}
